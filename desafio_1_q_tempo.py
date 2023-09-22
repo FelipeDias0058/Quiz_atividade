44dbc5d02d78c6f1b701329b8d3e05e8d085eb81
FelipeDias0058 <143054608+FelipeDias0058@users.noreply.github.com>
2023-09-22 01:29:23 -0300

    Add files via upload

---
+++ desafio_1_q_tempo.py
@@ -0,0 +1,8 @@
+print(" Qual é a capital do Piauí? ")
+resposta = input().lower()
+print(resposta)
+
+if resposta = "teresina":
+    print(" Parabéns, você acertou! ")
+else:
+    print(" Você errou uma pergunta tão fácil... ")
---
+++ desafio_2_mult_escolha.py
@@ -0,0 +1,47 @@
+print(''' De quem é a música 'Hey Jude'?
+a - Os Beatles
+b - Iron Maiden
+c - Rolling Stones
+''')
+
+resposta = input().lower()
+
+if resposta == "a":
+    print(" Parabéns, você acertou! ")
+elif resposta == "b" or "c":
+    print(" Tente novamente... ")
+else:
+    print("Responda com a, b ou c, por favor.")
+
+
+print(''' De quem é a música 'Paint it Black'?
+a - Os Beatles
+b - Iron Maiden
+c - Rolling Stones
+''')
+
+resposta = input().lower()
+
+if resposta == "c":
+    print(" Parabéns, você acertou! ")
+elif resposta == "a" or "b":
+    print(" Tente novamente... ")
+else:
+    print("Responda com a, b ou c, por favor.")
+    
+
+print(''' De quem é a música 'Fear of the Dark'?
+a - Os Beatles
+b - Iron Maiden
+c - Rolling Stones
+''')
+
+resposta = input().lower()
+
+if resposta == "b":
+    print(" Parabéns, você acertou! ")
+elif resposta == "a" or "c":
+    print(" Tente novamente... ")
+else:
+    print("Responda com a, b ou c, por favor.")
+
